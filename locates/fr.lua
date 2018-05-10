cda9c8aea388315c8516218d790f9d6dd5245a00
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2018-05-10 22:14:23 +0200

    lower case

--- locates/fr.lua
+++ locates/fr.lua
@@ -1,6 +1,6 @@
 Locales ['fr'] = {
-	['unlocked'] = '~g~Ouvert~s~',
-	['locked'] = '~r~Fermé~s~',
-	['not_cop'] = 'Vous n\avez pas les clés pour ouvrir cette porte',
+	['unlocked'] = '~g~ouvert~s~',
+	['locked'] = '~r~fermé~s~',
+	['not_cop'] = 'vous n\avez pas les clés pour ouvrir cette porte',
 	['press_button'] = '[E] ',
 }
