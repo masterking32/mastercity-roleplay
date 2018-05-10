72528fd0d0769818bc5fedf69793dee3b2a02e06
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2018-05-10 22:16:20 +0200

    last update

--- locates/fr.lua
+++ locates/fr.lua
@@ -1,6 +1,6 @@
 Locales ['fr'] = {
-	['unlocked'] = '~g~ouvert~s~',
-	['locked'] = '~r~fermé~s~',
+	['unlocked'] = '~g~Ouvert~s~',
+	['locked'] = '~r~Fermé~s~',
 	['not_cop'] = 'vous n\avez pas les clés pour ouvrir cette porte',
 	['press_button'] = '[E] ',
 }
