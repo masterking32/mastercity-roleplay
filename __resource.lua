8f6fb8a86b0a41c843bcfa5cf3e19815040e891c
ElPumpo <samo_kol@hotmail.com>
2018-05-10 14:11:13 +0200

    Re-wrote config & A LOT, made script modular

--- __resource.lua
+++ __resource.lua
@@ -1,10 +1,21 @@
--- https://wiki.fivem.net/wiki/Resource_manifest
-
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'Lock doors'
+description 'ESX lock doors'
 
-version '1.0.0'
+version '1.1.0'
 
-client_script 'client.lua'
-server_script 'server.lua'
+server_scripts {
+	'@es_extended/locale.lua',
+	'locates/en.lua',
+	'locates/sv.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locates/en.lua',
+	'locates/sv.lua',
+	'config.lua',
+	'client/main.lua'
+}
