807a6a0e9eb50ac74d8b0f6623d93c3a0b234156
syntasu <abouma18@hotmail.com>
2018-10-31 08:49:26 -0700

    Fix: primary key cannot be DEFAULT NULL.

--- esplugin_mysql.sql
+++ esplugin_mysql.sql
@@ -18,7 +18,7 @@ USE `essentialmode`;
 
 -- Dumping structure for table essentialmode.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `identifier` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `identifier` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `license` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
   `money` int(11) DEFAULT NULL,
   `bank` int(11) DEFAULT NULL,
