b9853d5e19fa9a98bf06062cc8a569192a367ecf
BronxDre <33867736+BronxDre@users.noreply.github.com>
2019-07-06 13:36:54 -0400

    Update items.lua
    Added xPlayer.removeInventoryItem to remove item after use

--- server/items.lua
+++ server/items.lua
@@ -6,11 +6,15 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 ESX.RegisterUsableItem('gauze', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
+	xPlayer.removeInventoryItem('gauze', 1)
+
 	TriggerClientEvent('mythic_hospital:items:gauze', source)
 end)
 
 ESX.RegisterUsableItem('bandage', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('bandage', 1)
 
 	TriggerClientEvent('mythic_hospital:items:bandage', source)
 end)
@@ -18,11 +22,15 @@ end)
 ESX.RegisterUsableItem('firstaid', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
+	xPlayer.removeInventoryItem('firstaid', 1)
+
 	TriggerClientEvent('mythic_hospital:items:firstaid', source)
 end)
 
 ESX.RegisterUsableItem('medkit', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('medkit', 1)
 
 	TriggerClientEvent('mythic_hospital:items:medkit', source)
 end)
@@ -30,17 +38,23 @@ end)
 ESX.RegisterUsableItem('vicodin', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
+	xPlayer.removeInventoryItem('vicodin', 1)
+
 	TriggerClientEvent('mythic_hospital:items:vicodin', source)
 end)
 
 ESX.RegisterUsableItem('hydrocodone', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
+	xPlayer.removeInventoryItem('hydrocodone', 1)
+
 	TriggerClientEvent('mythic_hospital:items:hydrocodone', source)
 end)
 
 ESX.RegisterUsableItem('morphine', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('morphine', 1)
 
 	TriggerClientEvent('mythic_hospital:items:morphine', source)
 end)
