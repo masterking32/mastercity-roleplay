e59457aac36683cc16a2581a83cc58cd3d1a8b7c
Alzar <srichardson97402@hotmail.com>
2019-09-10 04:32:48 -0700

    Fix cancelling breaking the bar if you immediately start another task

--- html/js/script.js
+++ html/js/script.js
@@ -1,7 +1,10 @@
+var cancelledTimer = null;
+
 $('document').ready(function() {
     MythicProgBar = {};
 
     MythicProgBar.Progress = function(data) {
+        clearTimeout(cancelledTimer);
         $(".progress-container").css({"display":"block"});
         $("#progress-label").text(data.label);
         $("#progress-bar").stop().css({"width": 0, "background-color": "#8b1014"}).animate({
@@ -23,7 +26,7 @@ $('document').ready(function() {
         $("#progress-label").text("CANCELLED");
         $("#progress-bar").stop().css( {"width": "100%", "background-color": "#2e2e2e"});
 
-        setTimeout(function () {
+        cancelledTimer = setTimeout(function () {
             $(".progress-container").css({"display":"none"});
             $("#progress-bar").css("width", 0);
             $.post('http://mythic_progbar/actionCancel', JSON.stringify({
