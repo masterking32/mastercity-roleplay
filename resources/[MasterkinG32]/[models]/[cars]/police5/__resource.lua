2dd591bffb21a8f0e62d0ba8bb6910e6a23969b1
Amin.MasterkinG <Lichwow_masterking@yahoo.com>
2020-10-02 07:39:23 +0330

    Cars

---
+++ resources/[MasterkinG32]/[models]/[cars]/police5/__resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+ 
+files {
+    'vehicles.meta',
+    'carvariations.meta'
+}
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'  -- Not Required
+
+
---
+++ resources/[MasterkinG32]/[models]/[cars]/policemustang/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+ 
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'dlctext.meta'
+}
+
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'  -- Not Required
+data_file 'DLCTEXT_METADATA_FILE' 'dlctext.meta'
+
+
