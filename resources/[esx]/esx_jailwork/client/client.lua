9024e21904578a19e4452a67768809d31d5473f5
Amin.MasterkinG <Lichwow_masterking@yahoo.com>
2020-10-02 07:35:35 +0330

    I think its from Sunrise server, But I changed that to working with essentialmode

---
+++ resources/[esx]/esx_jailwork/client/client.lua
@@ -0,0 +1,476 @@
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+local first = true
+ESX = nil
+
+PlayerData = {}
+
+local jailTime = 0
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		
+		Citizen.Wait(0)
+	end
+
+	while ESX.GetPlayerData() == nil do
+		
+		Citizen.Wait(10)
+	end
+	
+	if first then
+		ESX.SetPlayerData('jailed',0)
+		first = false
+	end
+	PlayerData = ESX.GetPlayerData()
+
+	LoadTeleporters()
+end)
+
+RegisterNetEvent("esx:playerLoaded")
+AddEventHandler("esx:playerLoaded", function(newData)
+	PlayerData = newData
+
+	Citizen.Wait(25000)
+
+	ESX.TriggerServerCallback("esx-qalle-jail:retrieveJailTime", function(inJail, newJailTime)
+		if inJail then
+
+			jailTime = newJailTime
+
+			JailLogin()
+		end
+	end)
+end)
+
+RegisterNetEvent("esx:setJob")
+AddEventHandler("esx:setJob", function(response)
+	PlayerData["job"] = response
+end)
+
+RegisterNetEvent("esx-qalle-jail:openJailMenu")
+AddEventHandler("esx-qalle-jail:openJailMenu", function()
+	OpenJailMenu()
+end)
+
+RegisterNetEvent("esx-qalle-jail:jailPlayer")
+AddEventHandler("esx-qalle-jail:jailPlayer", function(newJailTime)
+	TriggerEvent("esx_policejob:removeHandcuffFull")
+	ESX.SetPlayerData('jailed',1)
+
+	jailTime = newJailTime
+	Cutscene()
+end)
+
+RegisterNetEvent("esx-qalle-jail:unJailPlayer")
+AddEventHandler("esx-qalle-jail:unJailPlayer", function()
+	jailTime = 0
+
+	UnJail()
+end)
+
+function JailLogin()
+	local JailPosition = Config.JailPositions["Cell"]
+	SetEntityCoords(PlayerPedId(), JailPosition["x"], JailPosition["y"], JailPosition["z"] - 1)
+
+	ESX.ShowNotification("Akharin bar ke DC kardid too zendan boodid, bara hamin be zendan bazgashtid!")
+
+	InJail()
+end
+
+function UnJail()
+	InJail()
+
+	ESX.Game.Teleport(PlayerPedId(), Config.Teleports["Boiling Broke"])
+	ESX.SetPlayerData('jailed',0)
+
+	ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin)
+		TriggerEvent('skinchanger:loadSkin', skin)
+	end)
+
+	ESX.ShowNotification("~g~~h~Shoma Azad Shodid!")
+end
+
+function InJail()
+
+	--Jail Timer--
+
+	Citizen.CreateThread(function()
+
+		while jailTime > 0 do
+			
+			jailTime = jailTime - 1
+
+			ESX.ShowNotification("Shoma ~r~" .. jailTime .. "~w~ Daghighe digar azad mishavid!")
+
+			TriggerServerEvent("esx-qalle-jail:updateJailTime", jailTime)
+
+			if jailTime == 0 then
+				UnJail()
+
+				TriggerServerEvent("esx-qalle-jail:updateJailTime", 0)
+			end
+
+			Citizen.Wait(60000)
+		end
+
+	end)
+
+	--Jail Timer--
+	
+	--Check if player is dead--
+	Citizen.CreateThread(function()
+		while jailTime > 0 do
+
+			-- Check if player is dead then revive --
+			ESX.TriggerServerCallback('esx_jailer:getDeathStatus', function(isDead)
+				if isDead then
+					TriggerEvent('esx_ambulancejob:revive', -1)
+				end
+			end)
+
+			Citizen.Wait(2000)
+		end
+
+	end)
+	--Check if player is dead--
+
+	--Check Coordinates and disbale hot keys--
+    Citizen.CreateThread(function()
+		local playerPed = PlayerPedId()
+        local JailPosition = Config.JailPositions["Cell"]
+		while jailTime > 0 do
+
+			DisableControlAction(0, Keys['F1'],true)
+			DisableControlAction(0, Keys['F3'],true)
+			DisableControlAction(0, Keys['F5'],true)
+
+			-- end of that fucking shit --
+			if GetDistanceBetweenCoords(GetEntityCoords(playerPed), JailPosition.x, JailPosition.y, JailPosition.z) > 90 then
+				ESX.Game.Teleport(playerPed, JailPosition)
+				ESX.ShowNotification("~r~~h~Shoma nemitavanid az zendan farar konid.")
+			end
+
+			Citizen.Wait(0)
+		end
+
+	end)
+	--Check Coordinates and disbale hot keys--
+
+	--Prison Work--
+
+	Citizen.CreateThread(function()
+		while jailTime > 0 do
+			
+			local sleepThread = 500
+
+			local Packages = Config.PrisonWork["Packages"]
+
+			local Ped = PlayerPedId()
+			local PedCoords = GetEntityCoords(Ped)
+
+			for posId, v in pairs(Packages) do
+
+				local DistanceCheck = GetDistanceBetweenCoords(PedCoords, v["x"], v["y"], v["z"], true)
+
+				if DistanceCheck <= 10.0 then
+
+					sleepThread = 5
+
+					local PackageText = "Pack"
+
+					if not v["state"] then
+						PackageText = "Already Taken"
+					end
+
+					ESX.Game.Utils.DrawText3D(v, "[E] " .. PackageText, 0.4)
+
+					if DistanceCheck <= 1.5 then
+
+						if IsControlJustPressed(0, 38) then
+
+							if v["state"] then
+								PackPackage(posId)
+							else
+								ESX.ShowNotification("Shoma ghablan in package ro borde boodid")
+							end
+
+						end
+
+					end
+
+				end
+
+			end
+
+			Citizen.Wait(sleepThread)
+
+		end 
+	end)
+
+	--Prison Work--
+
+end
+
+Citizen.CreateThread(function()
+	
+	while true do
+		
+		if jailTime > 0 then
+			DisableControlAction(2, Keys['F1'], true)
+			Citizen.Wait(0)
+		else
+			Citizen.Wait(1000)
+		end
+	
+	end
+
+end)
+
+function LoadTeleporters()
+	Citizen.CreateThread(function()
+		while true do
+			
+			local sleepThread = 500
+
+			local Ped = PlayerPedId()
+			local PedCoords = GetEntityCoords(Ped)
+
+			for p, v in pairs(Config.Teleports) do
+
+				local DistanceCheck = GetDistanceBetweenCoords(PedCoords, v["x"], v["y"], v["z"], true)
+
+				if DistanceCheck <= 7.5 then
+
+					sleepThread = 5
+
+					ESX.Game.Utils.DrawText3D(v, "[E] baraye baz kardan e dar", 0.4)
+
+					if DistanceCheck <= 1.0 then
+						if IsControlJustPressed(0, 38) then
+							TeleportPlayer(v)
+						end
+					end
+				end
+			end
+
+			Citizen.Wait(sleepThread)
+
+		end
+	end)
+end
+
+function PackPackage(packageId)
+	local Package = Config.PrisonWork["Packages"][packageId]
+
+	LoadModel("prop_cs_cardbox_01")
+
+	local PackageObject = CreateObject(GetHashKey("prop_cs_cardbox_01"), Package["x"], Package["y"], Package["z"], true)
+
+	PlaceObjectOnGroundProperly(PackageObject)
+
+	TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_BUM_BIN", 0, false)
+
+	local Packaging = true
+	local StartTime = GetGameTimer()
+
+	while Packaging do
+		
+		Citizen.Wait(1)
+
+		local TimeToTake = 60000 * 1.20 -- Minutes
+		local PackPercent = (GetGameTimer() - StartTime) / TimeToTake * 100
+
+		if not IsPedUsingScenario(PlayerPedId(), "PROP_HUMAN_BUM_BIN") then
+			DeleteEntity(PackageObject)
+
+			ESX.ShowNotification("Canceled!")
+
+			Packaging = false
+		end
+
+		if PackPercent >= 100 then
+
+			Packaging = false
+
+			DeliverPackage(PackageObject)
+
+			Package["state"] = false
+		else
+			ESX.Game.Utils.DrawText3D(Package, "Packaging... " .. math.ceil(tonumber(PackPercent)) .. "%", 0.4)
+		end
+		
+	end
+end
+
+function DeliverPackage(packageId)
+	if DoesEntityExist(packageId) then
+		AttachEntityToEntity(packageId, PlayerPedId(), GetPedBoneIndex(PlayerPedId(),  28422), 0.0, -0.03, 0.0, 5.0, 0.0, 0.0, 1, 1, 0, 1, 0, 1)
+
+		ClearPedTasks(PlayerPedId())
+	else
+		return
+	end
+
+	local Packaging = true
+
+	LoadAnim("anim@heists@box_carry@")
+
+	while Packaging do
+
+		Citizen.Wait(5)
+
+		if not IsEntityPlayingAnim(PlayerPedId(), "anim@heists@box_carry@", "idle", 3) then
+			TaskPlayAnim(PlayerPedId(), "anim@heists@box_carry@", "idle", 8.0, 8.0, -1, 50, 0, false, false, false)
+		end
+
+		if not IsEntityAttachedToEntity(packageId, PlayerPedId()) then
+			Packaging = false
+			DeleteEntity(packageId)
+		else
+			local DeliverPosition = Config.PrisonWork["DeliverPackage"]
+			local PedPosition = GetEntityCoords(PlayerPedId())
+			local DistanceCheck = GetDistanceBetweenCoords(PedPosition, DeliverPosition["x"], DeliverPosition["y"], DeliverPosition["z"], true)
+
+			ESX.Game.Utils.DrawText3D(DeliverPosition, "[E] Leave Package", 0.4)
+
+			if DistanceCheck <= 2.0 then
+				if IsControlJustPressed(0, 38) then
+					DeleteEntity(packageId)
+					ClearPedTasksImmediately(PlayerPedId())
+					Packaging = false
+
+					TriggerServerEvent("esx-qalle-jail:prisonWorkReward")
+				end
+			end
+		end
+
+	end
+
+end
+
+function OpenJailMenu()
+	ESX.UI.Menu.Open(
+		'default', GetCurrentResourceName(), 'jail_prison_menu',
+		{
+			title    = "Menu ye zendan",
+			align    = 'center',
+			elements = {
+				{ label = "Jail Closest Person", value = "jail_closest_player" },
+				{ label = "Unjail Person", value = "unjail_player" }
+			}
+		}, 
+	function(data, menu)
+
+		local action = data.current.value
+
+		if action == "jail_closest_player" then
+
+			menu.close()
+
+			ESX.UI.Menu.Open(
+          		'dialog', GetCurrentResourceName(), 'jail_choose_time_menu',
+          		{
+            		title = "Zaman e zendan(be daghighe)"
+          		},
+          	function(data2, menu2)
+
+            	local jailTime = tonumber(data2.value)
+
+            	if jailTime == nil then
+              		ESX.ShowNotification("The time needs to be in minutes!")
+            	else
+              		menu2.close()
+
+              		local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+
+              		if closestPlayer == -1 or closestDistance > 3.0 then
+                		ESX.ShowNotification("Hich kas nazdik nist!")
+					else
+						ESX.UI.Menu.Open(
+							'dialog', GetCurrentResourceName(), 'jail_choose_reason_menu',
+							{
+							  title = "Dalil zendan"
+							},
+						function(data3, menu3)
+		  
+						  	local reason = data3.value
+		  
+						  	if reason == nil then
+								ESX.ShowNotification("Bayad dalil bezarid")
+						  	else
+								menu3.close()
+		  
+								local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+		  
+								if closestPlayer == -1 or closestDistance > 3.0 then
+								  	ESX.ShowNotification("Kasi nazdik nist!")
+								else
+								  	TriggerServerEvent("esx-qalle-jail:jailPlayer", GetPlayerServerId(closestPlayer), jailTime, reason)
+								end
+		  
+						  	end
+		  
+						end, function(data3, menu3)
+							menu3.close()
+						end)
+              		end
+
+				end
+
+          	end, function(data2, menu2)
+				menu2.close()
+			end)
+		elseif action == "unjail_player" then
+
+			local elements = {}
+
+			ESX.TriggerServerCallback("esx-qalle-jail:retrieveJailedPlayers", function(playerArray)
+
+				if #playerArray == 0 then
+					ESX.ShowNotification("Zendan e shoma khalist")
+					return
+				end
+
+				for i = 1, #playerArray, 1 do
+					table.insert(elements, {label = "Zendani: " .. playerArray[i].name .. " | Zaman e zendan: " .. playerArray[i].jailTime .. " daghighe", value = playerArray[i].identifier })
+				end
+
+				ESX.UI.Menu.Open(
+					'default', GetCurrentResourceName(), 'jail_unjail_menu',
+					{
+						title = "Azad kardan az zendan",
+						align = "center",
+						elements = elements
+					},
+				function(data2, menu2)
+
+					local action = data2.current.value
+
+					TriggerServerEvent("esx-qalle-jail:unJailPlayer", action)
+
+					menu2.close()
+
+				end, function(data2, menu2)
+					menu2.close()
+				end)
+			end)
+
+		end
+
+	end, function(data, menu)
+		menu.close()
+	end)	
+end
+
---
+++ resources/[esx]/esx_jailwork/server/server.lua
@@ -0,0 +1,195 @@
+ESX                = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterCommand("jail", function(src, args, raw)
+
+	local xPlayer = ESX.GetPlayerFromId(src)
+
+	if xPlayer["job"]["name"] == "police" then
+
+		local jailPlayer = args[1]
+		local jailTime = tonumber(args[2])
+		local jailReason = args[3]
+
+		if GetPlayerName(jailPlayer) ~= nil then
+
+			if jailTime ~= nil then
+				JailPlayer(jailPlayer, jailTime)
+
+				TriggerClientEvent("esx:showNotification", src, GetPlayerName(jailPlayer) .. " Zendani shod baraye ~r~~h~" .. jailTime .. " ~w~Daghighe!")
+				
+				if args[3] ~= nil then
+					GetRPName(jailPlayer, function(Firstname, Lastname)
+						TriggerClientEvent('chat:addMessage', -1, { args = { "^4[POLICE]^3 ",  Firstname .. " " .. Lastname .. " ^0Zendani shod be elate^8:^0 " .. args[3] }, color = { 249, 166, 0 } })
+					end)
+				end
+			else
+				TriggerClientEvent("esx:showNotification", src, "Zaman na motabar!")
+			end
+		else
+			TriggerClientEvent("esx:showNotification", src, "In shakhs online nist!")
+		end
+	else
+		TriggerClientEvent("esx:showNotification", src, "Shoma police nistid!")
+	end
+end)
+
+RegisterCommand("unjail", function(src, args)
+
+	local xPlayer = ESX.GetPlayerFromId(src)
+
+	if xPlayer["job"]["name"] == "police" then
+
+		local jailPlayer = args[1]
+
+		if GetPlayerName(jailPlayer) ~= nil then
+			UnJail(jailPlayer)
+		else
+			TriggerClientEvent("esx:showNotification", src, "In shakhs online nist!")
+		end
+	else
+		TriggerClientEvent("esx:showNotification", src, "Shoma police nistid!")
+	end
+end)
+
+RegisterServerEvent("esx-qalle-jail:jailPlayer")
+AddEventHandler("esx-qalle-jail:jailPlayer", function(targetSrc, jailTime, jailReason)
+	local src = source
+	local targetSrc = tonumber(targetSrc)
+    local xPlayer = ESX.GetPlayerFromId(src)
+
+	if xPlayer["job"]["name"] == "police" then
+		JailPlayer(targetSrc, jailTime)
+
+		GetRPName(targetSrc, function(Firstname, Lastname)
+			TriggerClientEvent('chat:addMessage', -1, { args = { "^4[POLICE]^3 ",  Firstname .. " " .. Lastname .. " ^0Zendani shod be elate^8:^0 " .. jailReason }, color = { 249, 166, 0 } })
+		end)
+
+		TriggerClientEvent("esx:showNotification", src, GetPlayerName(targetSrc) .. " Zendani shod baraye ~r~~h~" .. jailTime .. " ~w~Daghighe!")
+	else
+		return xPlayer.kick("Don't event think about it faggot!😈")
+	end
+end)
+
+RegisterServerEvent("esx-qalle-jail:unJailPlayer")
+AddEventHandler("esx-qalle-jail:unJailPlayer", function(targetIdentifier)
+	local src = source
+	local xPlayer = ESX.GetPlayerFromIdentifier(targetIdentifier)
+
+	if xPlayer ~= nil then
+		UnJail(xPlayer.source)
+	else
+		MySQL.Async.execute(
+			"UPDATE users SET jail = @newJailTime WHERE identifier = @identifier",
+			{
+				['@identifier'] = targetIdentifier,
+				['@newJailTime'] = 0
+			}
+		)
+	end
+
+	TriggerClientEvent("esx:showNotification", src, xPlayer.name .. " Azad shod!")
+end)
+
+RegisterServerEvent("esx-qalle-jail:updateJailTime")
+AddEventHandler("esx-qalle-jail:updateJailTime", function(newJailTime)
+	local src = source
+
+	EditJailTime(src, newJailTime)
+end)
+
+RegisterServerEvent("esx-qalle-jail:prisonWorkReward")
+AddEventHandler("esx-qalle-jail:prisonWorkReward", function()
+	local src = source
+
+	local xPlayer = ESX.GetPlayerFromId(src)
+
+	xPlayer.addMoney(math.random(13, 21))
+
+	TriggerClientEvent("esx:showNotification", src, "Mamnoon ye kam pool bara ghaza gereftid")
+end)
+
+function JailPlayer(jailPlayer, jailTime)
+	TriggerClientEvent("esx-qalle-jail:jailPlayer", jailPlayer, jailTime)
+
+	EditJailTime(jailPlayer, jailTime)
+end
+
+function UnJail(jailPlayer)
+	TriggerClientEvent("esx-qalle-jail:unJailPlayer", jailPlayer)
+
+	EditJailTime(jailPlayer, 0)
+end
+
+function EditJailTime(source, jailTime)
+
+	local src = source
+
+	local xPlayer = ESX.GetPlayerFromId(src)
+	local Identifier = xPlayer.identifier
+
+	MySQL.Async.execute(
+       "UPDATE users SET jail = @newJailTime WHERE identifier = @identifier",
+        {
+			['@identifier'] = Identifier,
+			['@newJailTime'] = tonumber(jailTime)
+		}
+	)
+end
+
+function GetRPName(playerId, data)
+	local Identifier = ESX.GetPlayerFromId(playerId).identifier
+
+	MySQL.Async.fetchAll("SELECT firstname, lastname FROM users WHERE identifier = @identifier", { ["@identifier"] = Identifier }, function(result)
+
+		data(result[1].firstname, result[1].lastname)
+
+	end)
+end
+
+ESX.RegisterServerCallback("esx-qalle-jail:retrieveJailedPlayers", function(source, cb)
+	
+	local jailedPersons = {}
+
+	MySQL.Async.fetchAll("SELECT firstname, lastname, jail, identifier FROM users WHERE jail > @jail", { ["@jail"] = 0 }, function(result)
+
+		for i = 1, #result, 1 do
+			table.insert(jailedPersons, { name = result[i].firstname .. " " .. result[i].lastname, jailTime = result[i].jail, identifier = result[i].identifier })
+		end
+
+		cb(jailedPersons)
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_jailer:getDeathStatus', function(source, cb)
+	local identifier = GetPlayerIdentifiers(source)[1]
+
+	MySQL.Async.fetchScalar('SELECT is_dead FROM users WHERE identifier = @identifier', {
+		['@identifier'] = identifier
+	}, function(isDead)
+		cb(isDead)
+	end)
+end)
+
+ESX.RegisterServerCallback("esx-qalle-jail:retrieveJailTime", function(source, cb)
+
+	local src = source
+
+	local xPlayer = ESX.GetPlayerFromId(src)
+	local Identifier = xPlayer.identifier
+
+
+	MySQL.Async.fetchAll("SELECT jail FROM users WHERE identifier = @identifier", { ["@identifier"] = Identifier }, function(result)
+
+		local JailTime = tonumber(result[1].jail)
+
+		if JailTime > 0 then
+
+			cb(true, JailTime)
+		else
+			cb(false, 0)
+		end
+
+	end)
+end)
